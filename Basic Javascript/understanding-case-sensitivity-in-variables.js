67b9a2bc9666bad32f0541aef01ad4dd930b2f5b
nnabicse <nnabi.cse@gmail.com>
2022-01-28 02:21:30 +0600

    understanding case sensitivity in variables

---
+++ Basic Javascript/understanding-case-sensitivity-in-variables.js
@@ -0,0 +1,11 @@
+//variable declaration
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// variable assignments
+
+var studlyCapVar =10;
+var properCamelCase = "A String";
+var titleCaseOver = 9000;
