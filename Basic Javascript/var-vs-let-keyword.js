510d9f8eff19c9f47d6994c51d28b1faf5fa899f
nnabicse <nnabi.cse@gmail.com>
2022-01-28 02:27:46 +0600

    var vs let keyword

---
+++ Basic Javascript/var-vs-let-keyword.js
@@ -0,0 +1,8 @@
+// variables declared with var's can override
+// variables declared with let's cannot override
+
+var catName = "Oliver";
+var catSaound = "Meow!"
+
+let catColor = "Green";
+let catBreed = "Jeep";
