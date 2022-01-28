7dbd7e88c84111bb82af3ea6760b9ebb4e197674
nnabicse <nnabi.cse@gmail.com>
2022-01-28 19:52:40 +0600

    const variable

---
+++ Basic Javascript/read-only-variables-with-const.js
@@ -0,0 +1,9 @@
+// declare & initialize variables with let & const
+
+let ffc = "freeCodeCamp";
+const FACT = "is cool";
+
+//cant change a const variable
+//FACT = "is awsome!";
+
+console.log(ffc, FACT);
