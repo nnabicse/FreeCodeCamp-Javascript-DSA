ecea96d10cffd2563d90f4d71f47de353bb4f122
nnabicse <nnabi.cse@gmail.com>
2022-01-31 01:37:36 +0600

    word blanks

---
+++ Basic Javascript/38_word-blanks.js
@@ -0,0 +1,10 @@
+//make sentences concatenating multiple variables
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb;
+
+console.log(wordBlanks);
