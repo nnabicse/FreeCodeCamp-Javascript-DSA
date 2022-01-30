3c50aa5d51a98c87faa0b04838578abc6fda826e
nnabicse <nnabi.cse@gmail.com>
2022-01-30 18:13:46 +0600

    finding the first char of a string using braket notation

---
+++ Basic Javascript/33_braket-notation-to-find-first-char-of-string.js
@@ -0,0 +1,8 @@
+//find the first character of a string using baket notation
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0];
+
+console.log(firstLetterOfLastName);
