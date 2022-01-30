90369df25108e58427ae78ea4276308efb213c04
nnabicse <nnabi.cse@gmail.com>
2022-01-30 18:19:12 +0600

    understanding strings immutibility

---
+++ Basic Javascript/34_understanding-string-immutibility.js
@@ -0,0 +1,10 @@
+let myStr = "Jello World";
+
+//this line below will not work, cause we cant change an individual char of a steing
+myStr[0] = "H";
+
+//if we want to change , we must change the whole string like below
+
+myStr = "Hello World"
+
+console.log(myStr);
