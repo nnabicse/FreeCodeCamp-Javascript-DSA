61ce65b9ec11c3140b9ebd7829999b94bd2082d8
nnabicse <nnabi.cse@gmail.com>
2022-01-30 17:41:16 +0600

    finding length of strings

---
+++ Basic Javascript/32_finding-length-of-string.js
@@ -0,0 +1,11 @@
+// find length of string
+
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+//find the length of lastName and store in lastNameLength
+
+lastNameLength = lastName.length;
+
+console.log(lastNameLength);
