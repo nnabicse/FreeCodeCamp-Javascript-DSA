3d5dcd9078f797e6697f8f8d7429e75d4d6fa9cd
nnabicse <nnabi.cse@gmail.com>
2022-01-30 17:37:29 +0600

    appending variables to strings

---
+++ Basic Javascript/31_appending-variables-to-strings.js
@@ -0,0 +1,11 @@
+//define a const and a let variable with strings
+
+const someAdjective = "Great";
+
+let myStr = "Learning code is ";
+
+// append someAdjective to myStr
+
+myStr += someAdjective;
+
+console.log(myStr);
