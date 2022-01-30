4645dff490015e3aa3d11dc44191126186cd998a
nnabicse <nnabi.cse@gmail.com>
2022-01-30 19:17:43 +0600

    finding nth to last character of a string

---
+++ Basic Javascript/37_finding-the-nth-to-last-char-in-strings.js
@@ -0,0 +1,7 @@
+//find the nth to last char in a string
+
+const lastName = "Lovelace";
+
+const secondToLastLetterOfLastName = lastName[lastName.length-2];
+
+console.log(secondToLastLetterOfLastName);
