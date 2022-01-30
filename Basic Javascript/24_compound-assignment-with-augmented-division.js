0ce8ddbab9a85ab3c0945748dc4b587e4c8b687a
nnabicse <nnabi.cse@gmail.com>
2022-01-30 16:18:48 +0600

    escape quote literals in strings

---
+++ Basic Javascript/24_compound-assignment-with-augmented-division.js
@@ -0,0 +1,13 @@
+//define some let variables
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+//modify using augmented division
+
+a /=12;
+b /= 4;
+c /= 11;
+
+console.log(a, b, c);
---
+++ Basic Javascript/25_escaping-literal-quote-in-strings.js
@@ -0,0 +1,5 @@
+//escape quote in string
+
+const myStr = "\"I am a double quote\" string inside \"double qoutes\"";
+
+console.log(myStr);
