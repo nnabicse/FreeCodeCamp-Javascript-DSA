37f5614de4caaea0f0970bf0a1b085b3edb93cb7
nnabicse <nnabi.cse@gmail.com>
2022-01-30 19:09:42 +0600

    finding last character of a string

---
+++ Basic Javascript/36_finding-last-char-in-string.js
@@ -0,0 +1,7 @@
+//find the last character of a string using braket notation
+
+const lastName = "Lovelace";
+
+const lastLetterOfLastName = lastName[lastName.length-1];
+
+console.log(lastLetterOfLastName);
