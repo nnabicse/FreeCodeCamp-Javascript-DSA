bd70e78fe7ba1dcef2a9bb1227e9a4770d9e9774
nnabicse <nnabi.cse@gmail.com>
2022-01-31 03:05:37 +0600

    reusable javascript function

---
+++ Basic Javascript/50_reusable-JS-functions.js
@@ -0,0 +1,7 @@
+//Create a function called reusableFunction which prints the string Hi World to the dev console.
+function reusableFunction() {
+    console.log("Hello World");
+    
+}
+
+reusableFunction();
