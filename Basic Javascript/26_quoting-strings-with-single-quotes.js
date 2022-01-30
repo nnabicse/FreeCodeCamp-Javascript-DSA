ad05a3227b50b79c809f4be41e8115277ce0c0d6
nnabicse <nnabi.cse@gmail.com>
2022-01-30 16:23:43 +0600

    quoting strings with single quotation

---
+++ Basic Javascript/26_quoting-strings-with-single-quotes.js
@@ -0,0 +1,5 @@
+//define a constant variable with anchor tag in it and quote with single quotation
+
+const myStr = '<a href=\"http://www.example.com\" target=\"_blank\">Link</a>';
+
+console.log(myStr);
