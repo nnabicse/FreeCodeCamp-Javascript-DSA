6c7e6cb7f070623c0d37a6c5c6c1aa3b45550063
nnabicse <nnabi.cse@gmail.com>
2022-01-30 15:53:26 +0600

    compound addition

---
+++ Basic Javascript/21_compound-assignment-with-augmented-addition.js
@@ -0,0 +1,13 @@
+//define some let variables and modify them using compound assignment
+
+let a =3;
+let b = 17;
+let c = 12;
+
+//modify using compound addiotion
+
+a += 12;
+b +=9;
+c += 7;
+
+console.log(a, b, c);
