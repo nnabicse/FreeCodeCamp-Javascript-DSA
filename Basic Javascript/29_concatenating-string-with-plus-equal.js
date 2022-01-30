a1c4c54460dacacd48ad21012b8379aeafce2e34
nnabicse <nnabi.cse@gmail.com>
2022-01-30 16:41:38 +0600

    string concatenation using plus equal

---
+++ Basic Javascript/29_concatenating-string-with-plus-equal.js
@@ -0,0 +1,9 @@
+//define a let variable and assign a string on it
+
+let myStr = "This is the first sentence.";
+
+//concatenate with another string
+
+myStr += " This is the second sentence.";
+
+console.log(myStr);
