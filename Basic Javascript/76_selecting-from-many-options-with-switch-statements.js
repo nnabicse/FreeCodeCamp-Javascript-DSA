f053245355edc232df482a2e5e9343cc8104170c
nnabicse <nnabi.cse@gmail.com>
2022-02-01 01:28:49 +0600

    selecting from many options with switch staments

---
+++ Basic Javascript/76_selecting-from-many-options-with-switch-statements.js
@@ -0,0 +1,23 @@
+function caseInSwitch(val) {
+    let answer = "";
+    switch(val){
+        case 1:
+            answer="alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+        default:
+            answer = "Wrong input";
+    }
+    return answer;
+    
+}
+
+console.log(caseInSwitch(1));
