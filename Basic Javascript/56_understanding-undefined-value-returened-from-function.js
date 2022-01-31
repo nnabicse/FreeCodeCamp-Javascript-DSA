991ce0a03c9eec927ca35c6278f5d42a420c800b
nnabicse <nnabi.cse@gmail.com>
2022-01-31 17:31:22 +0600

    understanding undefined value returned from a function

---
+++ Basic Javascript/56_understanding-undefined-value-returened-from-function.js
@@ -0,0 +1,17 @@
+let sum = 0;
+
+function addThree() {
+    sum = sum+3;
+    console.log(sum);
+    
+}
+
+function addFive() {
+    sum = sum+5;
+    console.log(sum);
+    
+}
+
+
+console.log(addThree(3)); // will result undefined
+console.log(addFive(3)); // will result undefined
