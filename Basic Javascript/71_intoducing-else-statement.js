bf645d0548fe9a8e99d4ebdc5c76ef20f7805edb
nnabicse <nnabi.cse@gmail.com>
2022-02-01 00:19:11 +0600

    introducing else statement

---
+++ Basic Javascript/71_intoducing-else-statement.js
@@ -0,0 +1,15 @@
+function testElse(val) {
+    let result="";
+
+    if(val>5){
+        result =  "Bigger than 5";
+    }
+    else{
+        result = "5 or Smaller";
+    }
+
+    return result;
+}
+
+console.log(testElse(4));
+
