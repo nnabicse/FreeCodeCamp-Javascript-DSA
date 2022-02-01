4570bd0689ac9bc64851cd4f7161881c4354a085
nnabicse <nnabi.cse@gmail.com>
2022-02-01 15:16:46 +0600

    manipulationg complex objects

---
+++ Basic Javascript/92_manipulating-complex-objects.js
@@ -0,0 +1,29 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+
+  myMusic.push(
+      {
+          "artist":"NNbabi",
+          "title": "M.Engg",
+          "release_year": 1998,
+          "formats":[
+              "CD",
+              "Cassette",
+              "LP"
+          ]
+      }
+    );
+
+
+    console.log(myMusic);
