c1f9ab37d961a4a589b7360fa54f3e445c26e362
nnabicse <nnabi.cse@gmail.com>
2022-02-01 13:30:51 +0600

    delete properties to objects

---
+++ Basic Javascript/89_delete-properties-from-objects.js
@@ -0,0 +1,11 @@
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+
+delete myDog.tails;
+
+console.log(myDog);
