2d7030059e81eabc84311a85ac69dcdae35253c3
nnabicse <nnabi.cse@gmail.com>
2022-02-01 12:38:04 +0600

    access object properties with braket notation

---
+++ Basic Javascript/85_access-object-properties-with-braket-notation.js
@@ -0,0 +1,10 @@
+const testObj = {
+    "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+const entreeValue = testObj["an entree"];
+const drinkValue = testObj["the drink"];
+
+console.log(entreeValue, drinkValue);
