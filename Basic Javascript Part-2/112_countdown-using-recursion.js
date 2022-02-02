dfb274294e21f2fee22921f5abc1da4648165017
nnabicse <nnabi.cse@gmail.com>
2022-02-02 16:35:58 +0600

    countdown using recursion

---
+++ Basic Javascript Part-2/112_countdown-using-recursion.js
@@ -0,0 +1,13 @@
+function countdown(n){
+    if(n<1){
+      return [];
+    }
+    else{
+  
+      const array = countdown(n-1);
+      array.unshift(n);
+      return array;
+    }
+  }
+
+console.log(countdown(5));
